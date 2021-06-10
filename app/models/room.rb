6752d7743c2e24fccf0dba9e36db9f1a545d2516
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-06-10 16:19:20 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,7 +4,10 @@ class Room < ApplicationRecord
   belongs_to :category
 
   validates :category_id, presence: true
+  validates :name,        presence: true
 
   validates :category_id, numericality: { other_than: 1, message:'を選択してください' } 
 
+  has_many :room_users
+  has_many :users, through: :room_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new]
+  resources :rooms, only: [:index, :new, :create]
 end
