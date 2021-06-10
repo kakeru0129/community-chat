e7dc401f8bcecd3f9582a6dcfcfb02963bb8eb5a
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-06-10 13:18:23 +0900

    新規作成画面と一覧画面の表示

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,10 @@
 class Room < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message:'を選択してください' } 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:index, :new]
 end
