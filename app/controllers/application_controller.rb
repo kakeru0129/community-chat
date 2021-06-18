3fe084d178634e1f489e531a96414b9432e79ce5
kakeru0129 <82084029+kakeru0129@users.noreply.github.com>
2021-06-18 15:44:02 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'kakeru' && password == '0129'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+
   end
 
   def configure_permitted_parameters
